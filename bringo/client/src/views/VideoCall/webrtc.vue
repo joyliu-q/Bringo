50fe12495421f6b002e29e12cc51affcdb3a10cb
rahmanapyrr <rahmana.muhammad@bison.howard.edu>
2020-10-03 19:47:06 -0400

    mount scripts

--- bringo/client/src/views/VideoCall/webrtc.vue
+++ bringo/client/src/views/VideoCall/webrtc.vue
@@ -69,10 +69,165 @@
         default: null
       }
     },
-  }
+  mounted() {
+      var that = this;
 
+      this.rtcmConnection = new RTCMultiConnection();
+      this.rtcmConnection.socketURL = this.socketURL;
+      this.rtcmConnection.autoCreateMediaElement = false;
+      this.rtcmConnection.enableLogs = this.enableLogs;
+      this.rtcmConnection.session = {
+        audio: this.enableAudio,
+        video: this.enableVideo
+      };
+      this.rtcmConnection.sdpConstraints.mandatory = {
+        OfferToReceiveAudio: this.enableAudio,
+        OfferToReceiveVideo: this.enableVideo
+      };
+      if ((this.stunServer !== null) || (this.turnServer !== null)) {
+        this.rtcmConnection.iceServers = []; // clear all defaults
+      }
+      if (this.stunServer !== null) {
+        this.rtcmConnection.iceServers.push({
+          urls: this.stunServer
+        });
+      }
+      if (this.turnServer !== null) {
+        var parse = this.turnServer.split('%');
+        var username = parse[0].split('@')[0];
+        var password = parse[0].split('@')[1];
+        var turn = parse[1];
+        this.rtcmConnection.iceServers.push({
+          urls: turn,
+          credential: password,
+          username: username
+        });
+      }
+      this.rtcmConnection.onstream = function (stream) {
+        let found = that.videoList.find(video => {
+          return video.id === stream.streamid
+        })
+        if (found === undefined) {
+          let video = {
+            id: stream.streamid,
+            muted: stream.type === 'local'
+          };
+
+          that.videoList.push(video);
+
+          if (stream.type === 'local') {
+            that.localVideo = video;
+          }
+        }
+
+        setTimeout(function(){ 
+          for (var i = 0, len = that.$refs.videos.length; i < len; i++) {
+            if (that.$refs.videos[i].id === stream.streamid)
+            {
+              that.$refs.videos[i].srcObject = stream.stream;
+              break;
+            }
+          }
+        }, 1000);
+        
+        that.$emit('joined-room', stream.streamid);
+      };
+      this.rtcmConnection.onstreamended = function (stream) {
+        var newList = [];
+        that.videoList.forEach(function (item) {
+          if (item.id !== stream.streamid) {
+            newList.push(item);
+          }
+        });
+        that.videoList = newList;
+        that.$emit('left-room', stream.streamid);
+      };
+    },
+    methods: {
+      join() {
+         var that = this;
+         this.rtcmConnection.openOrJoin(this.roomId, function (isRoomExist, roomid) {
+          if (isRoomExist === false && that.rtcmConnection.isInitiator === true) {
+            that.$emit('opened-room', roomid);
+          }
+        });
+      },
+      leave() {
+        this.rtcmConnection.attachStreams.forEach(function (localStream) {
+          localStream.stop();
+        });
+        this.videoList = [];
+      },
+      capture() {
+        return this.getCanvas().toDataURL(this.screenshotFormat);
+      },
+      getCanvas() {
+        let video = this.getCurrentVideo();
+        if (video !== null && !this.ctx) {
+          let canvas = document.createElement('canvas');
+          canvas.height = video.clientHeight;
+          canvas.width = video.clientWidth;
+          this.canvas = canvas;
+          this.ctx = canvas.getContext('2d');
+        }
+        const { ctx, canvas } = this;
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        return canvas;
+      },
+      getCurrentVideo() {
+        if (this.localVideo === null) {
+          return null;
+        }
+        for (var i = 0, len = this.$refs.videos.length; i < len; i++) {
+          if (this.$refs.videos[i].id === this.localVideo.id)
+            return this.$refs.videos[i];
+        }
+        return null;
+      },
+      shareScreen() {
+        var that = this;
+        if (navigator.getDisplayMedia || navigator.mediaDevices.getDisplayMedia) {
+          function addStreamStopListener(stream, callback) {
+            var streamEndedEvent = 'ended';
+            if ('oninactive' in stream) {
+                streamEndedEvent = 'inactive';
+            }
+            stream.addEventListener(streamEndedEvent, function() {
+                callback();
+                callback = function() {};
+            }, false);
+          }
+
+          function onGettingSteam(stream) {
+            that.rtcmConnection.addStream(stream);
+            that.$emit('share-started', stream.streamid);
+
+            addStreamStopListener(stream, function() {
+              that.rtcmConnection.removeStream(stream.streamid);
+              that.$emit('share-stopped', stream.streamid);
+            });
+          }
+
+          function getDisplayMediaError(error) {
+            console.log('Media error: ' + JSON.stringify(error));
+          }
+
+          if (navigator.mediaDevices.getDisplayMedia) {
+            navigator.mediaDevices.getDisplayMedia({video: true, audio: false}).then(stream => {
+              onGettingSteam(stream);
+            }, getDisplayMediaError).catch(getDisplayMediaError);
+          }
+          else if (navigator.getDisplayMedia) {
+            navigator.getDisplayMedia({video: true}).then(stream => {
+              onGettingSteam(stream);
+            }, getDisplayMediaError).catch(getDisplayMediaError);
+          }
+        }
+      }
+    }
+  };
   </script>
-  
+
 <style scoped>
   .video-list {
     background: rgb(228, 191, 69);
