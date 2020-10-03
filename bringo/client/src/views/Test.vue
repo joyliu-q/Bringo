0dad74b95035e726657d07a593281eb57e99317c
Qijia "Joy" Liu <joyliu.q@gmail.com>
2020-10-03 15:59:46 -0700

    font change to Helvetica and speechtotext config on blog

--- bringo/client/src/views/Test.vue
+++ bringo/client/src/views/Test.vue
@@ -1,7 +1,78 @@
 <template>
     <wrapper>
         <div>
-            LOL
+            <v-content>
+                <v-container >
+                    <!--Profile-->
+                    <v-card
+                        :loading="loading"
+                        class="mx-auto my-12"
+                        max-width="1000"
+                        height = "auto"
+                    >
+                        <v-card-title>
+                            Create New Blog Entry
+                            <v-spacer></v-spacer>
+                        </v-card-title>
+                        <v-card-text>
+                            <v-chip-group
+                                v-model="selection"
+                                active-class="deep-purple accent-4 white--text"
+                                column
+                            >
+                                <v-row align="center">
+                                    <v-card-title>Topics: </v-card-title>
+                                    <v-chip v-for="interest in interests" :key="interest"
+                                    >
+                                        {{interest}}
+                                    </v-chip>
+                                    <v-fab-transition>
+                                    <v-chip 
+                                        v-if="editProfile"
+                                        id="newChipButton"
+                                        @click="displayNewChip()"
+                                    >
+                                        <input 
+                                            type="text" 
+                                            id="newChipInput" 
+                                            placeholder="Add New Label Here" 
+                                            style="display:none"
+                                            @keydown.enter="addNewChip()"
+                                        >
+                                        <v-icon>mdi-plus</v-icon>
+                                    </v-chip>
+                                    </v-fab-transition>
+                                </v-row>
+                            </v-chip-group>
+                            <!--Speech to Text Button-->
+                            <v-btn 
+                                flat color="deep-purple accent-4 white--text" 
+                                @click="toggleSpeech()"
+                                style="float: left"
+                            >
+                                <v-icon>{{ speechOn ? 'mdi-microphone-outline' : 'mdi-microphone'}}</v-icon>
+                                {{ speechOn ? 'Pause/Stop: I\'m Finished' : 'Speech to Text: Too Tired to Type? Tell Your Story' }}
+                            </v-btn>
+                        </v-card-text>
+                    </v-card>
+                    <v-textarea
+                        solo
+                        name="input-7-4"
+                        label="Write your story here or tell it using our speech-to-text button above."
+                        rows="10"
+                        auto-grow
+                        counter
+                        :value="comment"
+                    ></v-textarea>
+                    <!--Publish Button-->
+                    <v-btn large 
+                        flat color="deep-purple accent-4 white--text" 
+                        style="float: right"
+                    >
+                        Publish Blog Entry
+                    </v-btn>
+                </v-container>
+            </v-content>
             <div id="app">
                 <v-app id="inspire">
                     <v-container fluid>
@@ -15,9 +86,6 @@
                         </v-flex>
                         <v-flex xs12 sm8 md4 text-xs-center>
                         <speech-to-text :text.sync="text" @speechend="speechEnd"></speech-to-text>
-                        </v-flex>
-                        <v-flex xs12 text-xs-center class="mt-4">
-                        {{sentences}}
                         </v-flex>
                     </v-layout>
                     </v-container>
