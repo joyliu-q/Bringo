ce4720ec101c0cf34f8ad6891b672c900f0228d9
Qijia "Joy" Liu <joyliu.q@gmail.com>
2020-10-03 13:49:36 -0700

    dashboard changes and adding list display, as well as overlays

--- bringo/client/src/views/Dashboard.vue
+++ bringo/client/src/views/Dashboard.vue
@@ -2,29 +2,89 @@
     <wrapper>
         <v-content>
             <v-overlay :value="overlay">
-                <v-card v-if="overlay_page==0">
+                <v-card v-if="overlay_page==0" max-width="500">
                     <v-card-title>Hi! Welcome to Bringo.</v-card-title>
                     <v-card-text>Before we start, let me give you a rundown of how our platform works!</v-card-text>
-                    <v-btn @click="overlay_page+=1"></v-btn>
+                    <v-row justify="center">
+                        <v-btn large @click="overlay_page+=1">Okay</v-btn>
+                    </v-row>
                 </v-card>
-                <v-card v-if="overlay_page==1">
+                <v-card v-if="overlay_page==1" max-width="500">
                     <v-card-title>Dashboard</v-card-title>
                     <v-card-text>This is the dashboard page. This is the page you will see after logging on. On the dashboard page, there are 3 main sections: profile, blog, and social.</v-card-text>
-                    <v-btn @click="overlay_page+=1"></v-btn>
+                    <v-row justify="center">
+                        <v-btn large @click="overlay_page+=1">Okay</v-btn>
+                    </v-row>
                 </v-card>
-                <v-card v-if="overlay_page==2">
+                <v-card v-if="overlay_page==2" max-width="500">
                     <v-card-title>Profile</v-card-title>
-                    <v-card-text>In the profile section, you can view and edit your information, such as your name, age, and interests. With this information, other people can find you through the directory and we can better pair you up with others for calls and social events.</v-card-text>
-                    <v-btn @click="overlay_page+=1"></v-btn>
+                    <v-card-text>In profile, you can view and edit your information, such as your name, age, and interests.</v-card-text>
+                    <v-row justify="center">
+                        <v-btn large @click="overlay_page+=1">Okay</v-btn>
+                    </v-row>
                 </v-card>
-                <v-card v-if="overlay_page==3">
+                <v-card v-if="overlay_page==3" max-width="500">
                     <v-card-title>Profile</v-card-title>
-                    <v-card-text>To complete the profile, we still need a few details from you. Let's fill it out together!</v-card-text>
+                    <v-card-text>With this information, other people can find you through the directory and we can better pair you up with others for calls and social events.</v-card-text>
+                    <v-row justify="center">
+                        <v-btn large @click="overlay_page+=1">Okay</v-btn>
+                    </v-row>
                 </v-card>
-                <v-card v-if="overlay_page==4">
-                    <v-card-title>Dashboard</v-card-title>
-                    <v-card-text>This is the dashboard page. When you registered for an account, we asked for your name, age, and interests. That infomation can be found in the profile section!</v-card-text>
-                    <v-btn @click="overlay_page+=1"></v-btn>
+                <v-card v-if="overlay_page==4" max-width="500">
+                    <v-card-title>Blog</v-card-title>
+                    <v-card-text>In Blog, you can share your personal stories and experiences. Our blog postings are designed to be user friendly: you can either type it or use the speech-to-text function!</v-card-text>
+                    <v-row justify="center">
+                        <v-btn large @click="overlay_page+=1">Okay</v-btn>
+                    </v-row>
+                </v-card>
+                <v-card v-if="overlay_page==5" max-width="500">
+                    <v-card-title>Blog</v-card-title>
+                    <v-card-text>Similarly, you can read others' blogs as well.</v-card-text>
+                    <v-row justify="center">
+                        <v-btn large @click="overlay_page+=1">Okay</v-btn>
+                    </v-row>
+                </v-card>
+                <v-card v-if="overlay_page==6" max-width="500">
+                    <v-card-title>Social</v-card-title>
+                    <v-card-text>In the social session, you can schedule calls and socialize with others, as well as viewing upcoming events. </v-card-text>
+                    <v-row justify="center">
+                        <v-btn large @click="overlay_page+=1">Okay</v-btn>
+                    </v-row>
+                </v-card>
+                <v-card v-if="overlay_page==7" max-width="500">
+                    <v-card-title>Bingo</v-card-title>
+                    <v-card-text>Lastly, our favourite section: Bingo!</v-card-text>
+                    <v-row justify="center">
+                        <v-btn large @click="overlay_page+=1">Okay</v-btn>
+                    </v-row>
+                </v-card>
+                <v-card v-if="overlay_page==8" max-width="500">
+                    <v-card-title>Bingo</v-card-title>
+                    <v-card-text>Once you finish a call, you can add a mark on your social Bingo sheet. (The goal is to meet all sorts of people and share all sorts of experiences, so you can reach a Bingo!)</v-card-text>
+                    <v-row justify="center">
+                        <v-btn large @click="overlay_page+=1">Okay</v-btn>
+                    </v-row>
+                </v-card>
+                <v-card v-if="overlay_page==9" max-width="500">
+                    <v-card-title>Bingo</v-card-title>
+                    <v-card-text>This Bingo sheet will also be updated monthly, so try to get them completed!</v-card-text>
+                    <v-row justify="center">
+                        <v-btn large @click="overlay_page+=1">Okay</v-btn>
+                    </v-row>
+                </v-card>
+                <v-card v-if="overlay_page==10" max-width="500">
+                    <v-card-title>Conclusion</v-card-title>
+                    <v-card-text>These all may sound like a lot of features: take your time to figure it out! For each page, we will have a tutorial where we can guide you through how to use this platform. You can find it by clicking "Help" on the top right.</v-card-text>
+                    <v-row justify="center">
+                        <v-btn large @click="overlay_page+=1">Okay</v-btn>
+                    </v-row>
+                </v-card>
+                <v-card v-if="overlay_page==11" max-width="500">
+                    <v-card-title>Conclusion</v-card-title>
+                    <v-card-text>Thank you for using Bringo! Enjoy :D</v-card-text>
+                    <v-row justify="center">
+                        <v-btn large @click="overlay=false">Okay</v-btn>
+                    </v-row>
                 </v-card>
             </v-overlay>
             <v-container>
@@ -93,27 +153,57 @@
                 </v-card>
                 <v-row justify="center">
                     <!--Blog-->
-                    <v-col cols="7">
-                        <v-card class="mx-auto my-12" height="300px">
-                            <v-card-title>Blog</v-card-title>
+                    <v-col cols="5">
+                        <v-card class="mx-auto my-12" height="350px">
+                            <v-card-title>
+                                <v-icon>mdi-pen</v-icon>
+                                Blog
+                            </v-card-title>
                             <v-card-text>My Stories</v-card-text>
                             <v-spacer></v-spacer>
-                            <v-btn>View Blog Page</v-btn>
-                            <v-btn to="create_blog">Write a New Entry</v-btn>
+                            <v-row class="ma-5">
+                                <v-btn>My Blog</v-btn>
+                                <v-btn to="create_blog">Write New Entry</v-btn>
+                            </v-row>
                             <v-spacer></v-spacer>
                             <v-card-text>Community Highlights</v-card-text>
                             <v-spacer></v-spacer>
-                            <v-btn>Browse Other Blogs</v-btn>
-                            <v-btn>Random Suggestion: Surprise Me</v-btn>
+                            <v-row class="ma-5">
+                                <v-btn>Browse Blogs</v-btn>
+                                <v-btn>Surprise Me</v-btn>
+                            </v-row>
                         </v-card>
                     </v-col>
                     <!--Sessions-->
-                    <v-col cols="5">
-                        <v-card class="mx-auto my-12" height="300px">
-                            <v-card-title>Schedule Discussion Sessions</v-card-title>
-                            <v-btn @click="alertCallRequests()">View Upcoming Calls</v-btn>
-                            <v-spacer></v-spacer>
-                            <v-btn to="directory">User Directory: Book New Call</v-btn>
+                    <v-col cols="7">
+                        <v-card class="mx-auto my-12" height="350px">
+                            <v-card-title>
+                                <v-icon>mdi-calendar-account</v-icon>
+                                Schedule Discussion Sessions
+                            </v-card-title>
+                            <v-list rounded>
+                                <v-subheader>
+                                    Upcoming Events
+                                </v-subheader>
+                                <v-list-item-group>
+                                    <v-list-item
+                                    v-for="event in events"
+                                    :key="event"
+                                    >
+                                    <v-list-item-icon>
+                                        <v-icon>mdi-clock</v-icon>
+                                    </v-list-item-icon>
+                                    <v-list-item-content>
+                                        <v-list-item-title v-text="event"></v-list-item-title>
+                                    </v-list-item-content>
+                                    </v-list-item>
+                                </v-list-item-group>
+                            </v-list>
+                            <v-row class="ma-5">
+                                <v-btn @click="alertCallRequests()">View Upcoming Calls</v-btn>
+                                <v-spacer></v-spacer>
+                                <v-btn to="directory">Book New Social Call</v-btn>
+                            </v-row>
                         </v-card>
                     </v-col>
                 </v-row>
@@ -136,6 +226,7 @@ export default {
             overlay_page: 0,
             editProfile: false,
             interests: ["Literature", "Book Illustration", "Roald Dahl"],
+            events: ["September 30 - Meet with Maria Li", "October 02 - Meet with Daniel Callaway"],
         }
     },
     methods: {
