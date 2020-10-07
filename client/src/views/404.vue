aa7f17fa5aafa22f4ce69c7970a9f74039d21fab
Qijia "Joy" Liu <joyliu.q@gmail.com>
2020-10-06 23:02:50 -0700

    added styling for error page

--- client/src/views/404.vue
+++ client/src/views/404.vue
@@ -1,7 +1,27 @@
 <template>
     <wrapper>
         <v-content>
-            <v-img src="../assets/404.png"></v-img>
+            <v-hover>
+                <template v-slot:default="{ hover }">
+                    <v-card
+                        class="mx-auto"
+                        max-width="100%"
+                    >
+                        <v-img src="../assets/404.svg"></v-img>
+                        <v-fade-transition>
+                            <v-overlay
+                                v-if="hover"
+                                absolute
+                                color="none"
+                            >
+                                <v-card-actions class="justify-center" style="height:300px">
+                                    <v-btn x-large class="returnButton" @click="returnPage">Return to Main</v-btn>
+                                </v-card-actions>
+                            </v-overlay>
+                        </v-fade-transition>
+                    </v-card>
+                </template>
+            </v-hover>
         </v-content>
     </wrapper>
 </template>
@@ -16,9 +36,25 @@ export default {
     },
     data: function () {
         return {
+            loggedIn: true,
         }
     },
     methods: {
+        returnPage () {
+            if (this.loggedIn == true) {
+                this.$router.push('dashboard');
+            }
+            else {
+                this.$router.push('/');
+            }
+        }
     },
 }
 </script>
+
+<style>
+    .returnButton{
+        width: 500px;
+        margin-top: 100px;
+    }
+</style>
